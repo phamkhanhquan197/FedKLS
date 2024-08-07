2a6de5c1de6f5b89b8b8e3274ee87e6ec7d99f5f
kundroomajid <kundroomajid@gmail.com>
2024-08-07 12:23:47 +0900

    renamed test_column to test_set

--- mak/utils/dataset_info.py
+++ mak/utils/dataset_info.py
@@ -4,41 +4,41 @@ dataset_info = { # Image datasets
                 'cifar10': {
                             'num_classes': 10,
                             'output_column': 'label',
-                            'test_column': 'test'
+                            'test_set': 'test'
                             },
                 'cifar100': {
                             'num_classes': 100,
                             'output_column': 'fine_label',
-                            'test_column': 'test'
+                            'test_set': 'test'
                             },
                 'mnist': {
                             'num_classes': 10,
                             'output_column': 'label',
-                            'test_column': 'test'
+                            'test_set': 'test'
                             },
                 'fashion_mnist': {
                             'num_classes': 10,
                             'output_column': 'label',
-                            'test_column': 'test'
+                            'test_set': 'test'
                             },
                 'sasha/dog-food': {
                             'num_classes': 2,
                             'output_column': 'label',
-                            'test_column': 'test'
+                            'test_set': 'test'
                             },
                 'zh-plus/tiny-imagenet': {
                             'num_classes': 200,
                             'output_column': 'label',
-                            'test_column': 'valid'
+                            'test_set': 'valid'
                             },
                 'Mike0307/MNIST-M': {
                             'num_classes': 10,
                             'output_column': 'label',
-                            'test_column': 'test'
+                            'test_set': 'test'
                             },
                 'flwrlabs/usps': {
                             'num_classes': 10,
                             'output_column': 'label',
-                            'test_column': 'test'
+                            'test_set': 'test'
                             },
                 }
