da118b0883369e54da6459d8c7e162f855daa7f9
Nguyễn Khoa <42170045+neko941@users.noreply.github.com>
2024-10-18 12:02:30 +0900

    update docs for losses

--- mak/losses/__init__.py
+++ mak/losses/__init__.py
@@ -1,10 +1,3 @@
-from torch.nn import CrossEntropyLoss as CE
+from .FocalLoss import FocalLoss
+from torch.nn import CrossEntropyLoss
 # from .LDAMLoss import LDAMLoss as LDAM
-from .FocalLoss import FocalLoss as Focal
-
-
-# # Automatically create a list of all classes imported in this file
-# import sys
-# import inspect
-# LOSSES = [name for name, obj in sys.modules[__name__].__dict__.items() if inspect.isclass(obj)]
-# print(f'{LOSSES = }')
