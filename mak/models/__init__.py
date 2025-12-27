aca1148408901fc8b8e31403a132ed0c831803dc
Hoang Vo~ <vohoang2204@gmail.com>
2025-12-27 15:40:55 +0700

    Update try except import

--- mak/models/__init__.py
+++ mak/models/__init__.py
@@ -15,10 +15,10 @@ try:
     from .efficientnet import EfficientNetB0
     from .mobilenet import MobileNetV2
     from .resnet_torch import ResNet18Pretrained, ResNet34Pretrained
+    from .resnet import Resnet18, Resnet34
 except Exception:
     EfficientNetB0 = None
 
 from .fedlaw_models import ResNet18Small, ResNet20Small
 from .lstm import LSTMModel
-from .resnet import Resnet18, Resnet34
 from .clip import Clip
