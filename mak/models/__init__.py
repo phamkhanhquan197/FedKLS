3c42b73d0fc3e5bd22926c61e6db7de8ad862bba
Hoang Vo~ <vohoang2204@gmail.com>
2025-12-27 15:40:00 +0700

    Update try-except for Kaggle

--- mak/models/__init__.py
+++ mak/models/__init__.py
@@ -13,12 +13,12 @@ from .cnn import (
 # Kaggle env bug workaround for EfficientNet import
 try:
     from .efficientnet import EfficientNetB0
+    from .mobilenet import MobileNetV2
+    from .resnet_torch import ResNet18Pretrained, ResNet34Pretrained
 except Exception:
     EfficientNetB0 = None
 
 from .fedlaw_models import ResNet18Small, ResNet20Small
 from .lstm import LSTMModel
-from .mobilenet import MobileNetV2
 from .resnet import Resnet18, Resnet34
-from .resnet_torch import ResNet18Pretrained, ResNet34Pretrained
 from .clip import Clip
