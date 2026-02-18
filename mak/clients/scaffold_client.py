c46e4985381e792c4b55e8ca9fdcd62fc108c93c
Khanh Quan <khanhquan@chungbuk.ac.kr>
2026-02-18 20:17:13 +0900

    Refactor ScaffoldClient to include new parameters

--- mak/clients/scaffold_client.py
+++ mak/clients/scaffold_client.py
@@ -16,10 +16,10 @@ class ScaffoldClient(BaseClient):
     """
 
     def __init__(
-        self, client_id, model, trainset, valset, config_sim, device, save_dir, dataset=None, apply_transforms=None, data_scheduler=None, bias=None, **kwargs
+        self, client_id, model, trainset, valset, config_sim, device, save_dir, dataset=None, apply_transforms=None, data_scheduler=None, bias=None, rank_policy_map=None, clip_collator=None,
     ):
         super().__init__(
-            client_id, model, trainset, valset, config_sim, device, save_dir, dataset=dataset, apply_transforms=apply_transforms, data_scheduler=data_scheduler, bias=bias, **kwargs
+            client_id, model, trainset, valset, config_sim, device, save_dir, dataset=dataset, apply_transforms=apply_transforms, data_scheduler=data_scheduler, bias=bias, rank_policy_map=rank_policy_map, clip_collator=clip_collator
         )
         # initialize client control variate with 0 and shape of the network parameters
         self.client_cv = []
